2919b87f2e6edaf4ec4d1bb92a21a7db267e7666
Mario Medina <mario.medina@it2xtreme.com>
2020-03-23 16:21:33 -0500

    Agrega ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  apps : [{
+  apps: [ {
     name: 'API',
     script: 'app.js',
     instances: 1,
     autorestart: true,
     watch: true,
+    ignore_watch: [ 'website', 'public', '.git', 'package.json', 'ecosystem.config.js' ],
     max_memory_restart: '1G',
+    node_args: [ '--expose-gc', '--max-old-space-size=1024' ],
     env: {
-      PORT: 80
-    },
-  }]
+      PORT: 80,
+      LD_PRELOAD: '/usr/lib/x86_64-linux-gnu/libjemalloc.so'
+    }
+  } ]
 };
