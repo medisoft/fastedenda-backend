be03a967e6b886230ab2adc174fe536fccf38282
Mario Medina <mario.medina@it2xtreme.com>
2020-03-23 19:56:33 -0500

    Bueno, con una local storage

--- controllers/items/index.js
+++ controllers/items/index.js
@@ -1,4 +1,4 @@
 exports.list = async (ctx, next) => {
-  const tasks = await ctx.pools.pg.query('SELECT i.*,b.name FROM items i, items_categories ic, categories c, brands b WHERE i.id=ic.item_id AND ic.category_id = c.id AND i.brand_id=b.id AND c.action = $1 ORDER BY i.name', [ ctx.params.category_id ]);
+  const tasks = await ctx.pools.pg.query('SELECT i.*,b.name AS brand FROM items i, items_categories ic, categories c, brands b WHERE i.id=ic.item_id AND ic.category_id = c.id AND i.brand_id=b.id AND c.action = $1 ORDER BY i.name', [ ctx.params.category_id ]);
   ctx.body = tasks.rows;
 };
