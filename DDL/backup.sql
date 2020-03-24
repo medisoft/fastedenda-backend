2eb747602da6e85e101395fbb47f20c139b367d9
Mario Medina <mario.medina@it2xtreme.com>
2020-03-24 01:09:27 -0500

    Bueno, con una local storage

--- DDL/backup.sql
+++ DDL/backup.sql
@@ -5,6 +5,7 @@
 -- Dumped from database version 10.12 (Ubuntu 10.12-0ubuntu0.18.04.1)
 -- Dumped by pg_dump version 10.12 (Ubuntu 10.12-0ubuntu0.18.04.1)
 
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
