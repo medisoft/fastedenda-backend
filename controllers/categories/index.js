27091771de9ac4b83ee4abca5c620d84033f60ba
Mario Medina <mario.medina@it2xtreme.com>
2020-03-23 19:42:37 -0500

    Bueno, con una local storage

--- controllers/categories/index.js
+++ controllers/categories/index.js
@@ -1,6 +1,7 @@
 exports.list = async (ctx, next) => {
   const tasks = await ctx.pools.pg.query('SELECT * FROM categories WHERE task_id = $1 AND parent_id IS NULL ORDER BY orderby', [ ctx.params.task_id ]);
+  console.log('aca',tasks)
   if (ctx.app.io) ctx.app.io.socket.emit('categoria-' + ctx.params.task_id, tasks.rows);
-  else ctx.body = tasks.rows;
+  ctx.body = tasks.rows;
   return tasks.rows;
 };
